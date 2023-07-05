dc50ac0b08d3333816cad88761f2fcb65a0faf0b
meninojohnny <120663121+meninojohnny@users.noreply.github.com>
2023-07-04 22:02:35 -0300

    Update dnaSequence.dart

--- dna_sequnce/lib/dnaSequence.dart
+++ dna_sequnce/lib/dnaSequence.dart
@@ -1,62 +1,40 @@
 import 'dart:math';
+import './utils.dart';
 
 class DNASequence {
-  String _dna = '';
+  String dna = '';
 
   DNASequence(String n) {
-    _dna = n.toUpperCase();
+    checkSequence(n);
+    dna = n.toUpperCase();
   }
 
-  factory DNASequence.fromAleatory(int comprimento) {
+  factory DNASequence.fromAleatory(int length) {
     var random = Random();
     String dnaSequence = '';
-    List<String> nucleotideos = ['A', 'T', 'C', 'G'];
-    for (int i = 0; i < comprimento; i++) {
-      dnaSequence += nucleotideos[random.nextInt(4)];
+    List<String> nucleotides = ['A', 'T', 'C', 'G'];
+    for (int i = 0; i < length; i++) {
+      dnaSequence += nucleotides[random.nextInt(4)];
     }
     return DNASequence(dnaSequence);
   }
 
-  DNASequence createInstance(String dna) {
-    String dnaInvertido = dna.split('').reversed.join();
-    return DNASequence(dnaInvertido);
+  DNASequence createInstance() {
+    checkSequence(dna);
+    print(dna);
+    String dnaInverted = dna.split('').reversed.join();
+    return DNASequence(dnaInverted);
   }
 
-  DNASequence createInstanceComplement(String dna) {
-    //String dnaInvertido = dna.split('').reversed.join();
-    List<String> dnaList = dna.split('');
-    for (int i = 0; i < dnaList.length; i++) {
-      switch (dnaList[i]) {
-        case 'A':
-          dnaList[i] = 'T';
-          break;
-        case 'T':
-          dnaList[i] = 'A';
-          break;
-        case 'C':
-          dnaList[i] = 'G';
-          break;
-        case 'G':
-          dnaList[i] = 'C';
-          break;
-      }
-      dna = dnaList.join();
-    }
-    return DNASequence(dna);
+  DNASequence createInstanceComplement() {
+    checkSequence(dna);
+    return DNASequence(complement(dna));
   }
 
   int occurrences(String nucleotideos) {
-    List<String> dnaList = _dna.split('');
+    checkNucleotide(nucleotideos);
+    List<String> dnaList = dna.split('');
     int count = dnaList.where((n) => n == nucleotideos.toUpperCase()).length;
     return count;
-  }
-
-  String get dna {
-    return _dna;
-  }
-
-  @override
-  String toString() {
-    return this.dna;
   }
 }
